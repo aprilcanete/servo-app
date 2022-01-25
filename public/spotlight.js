401a525b539ba183213d12a7e575afbc18c67233
April Canete <aprilrose.canete93@gmail.com>
2022-01-25 16:18:39 +1100

    added spotlight info box 2

--- public/spotlight.js
+++ public/spotlight.js
@@ -1,9 +1,23 @@
-const spotlightName = document.querySelector('.spotlight-name')
+const spotlightOwner = document.querySelector('.spotlight-owner')
+const spotlightLinkName = document.querySelector('.spotlight-link-name')
+const spotlightBtn = document.querySelector('.spotlight-btn')
+
 const url = 'http://localhost:8080/api/stations/random'
 
 axios
     .get(url)
     .then(res => {
-        
-        spotlightName.textContent = res.data[0].owner
+        spotlightLinkName.textContent = res.data[0].name
+        spotlightOwner.textContent = res.data[0].owner
     })
+
+
+spotlightBtn.addEventListener('click', (event) => {
+
+    axios
+    .get(url)
+    .then(res => {
+        spotlightLinkName.textContent = res.data[0].name
+        spotlightOwner.textContent = res.data[0].owner
+    })
+})
